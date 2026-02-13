12c4db9a23dd2739d87b471196afeb1d69656bcf
2sexyfortshirt <whosdefirst@gmail.com>
2026-02-13 16:36:07 -0500

    Added sentiment microservice integration

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,15 +2,19 @@
 # from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
 
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
+    path(route='login', view=views.login_user, name='login'),
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path('get_dealer/<str:dealer_id>', views.get_dealer_details, name='get_dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+        path(route='add_review', view=views.add_review, name='add_review'),
     # path for dealer reviews view
 
     # path for add a review view
